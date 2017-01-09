ba21c1728118368171daa60bfccdca4e23bc8e9b
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2017-01-09 21:22:30 +0530

    choose_one_basic component

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'speedyio',
+    packages = ['speedyio'],
+    version = '0.1',
+    description = 'Easy way to add terminal IO to your Python applications',
+    author = 'Arpit Bhayani',
+    author_email = 'arpit.b.bhayani@gmail.com',
+    url = 'https://github.com/arpitbbhayani/speedyio',
+    download_url = 'download link you saved',
+    keywords = ['io', 'cli'],
+    classifiers = [],
+)
