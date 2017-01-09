0ac4642cf89c785b9072115dc052930a07168feb
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2017-01-09 21:25:23 +0530

    Updating description and version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name = 'speedyio',
     packages = ['speedyio'],
-    version = '0.1',
-    description = 'Easy way to add terminal IO to your Python applications',
+    version = '0.0.1',
+    description = 'Input from console made easy and interactive for Python',
     author = 'Arpit Bhayani',
     author_email = 'arpit.b.bhayani@gmail.com',
     url = 'https://github.com/arpitbbhayani/speedyio',
