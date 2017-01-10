9a6c153dd882ab308d4f52c3a740de47783e07d1
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2017-01-10 20:59:22 +0530

    Removing debug messages

--- speedyio/basic/askfor.py
+++ speedyio/basic/askfor.py
@@ -1,7 +1,6 @@
-print("YO MESS")
 def askfor(t, message, empty_allowed=True):
     while True:
-        x = input(message + " : ")
+        x = input(message + ": ")
         if empty_allowed is True or x:
             # if empty string is allowed return it.
             # if it is not allowd and some value of x is provided then return
